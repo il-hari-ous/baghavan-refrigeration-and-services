9595a91c168c95a90651c12e0501b5de8b5a755c
HARIPRASAD V <120312783+il-hari-ous@users.noreply.github.com>
2024-10-09 15:22:20 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -120,9 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-MEDIA_URLS ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if DEBUG:
     STATICFILES_DIRS=[BASE_DIR/"static"]
 else:
